d415fae42478cfc102eeace2d3bbc635e79a4319
Gebruiker <Gebruiker@10.76.8.165>
2015-09-03 14:10:59 +0200

    hoofdstuk 2 opdracht 2

---
+++ src/hoofdstuk2/Opdracht2.java
@@ -0,0 +1,19 @@
+package hoofdstuk2;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht2 extends Applet {
+	
+
+	  public void init() {
+	    
+	  }
+
+	  public void paint(Graphics g) {
+	    g.setColor(Color.blue);
+	    g.drawString("Michael", 50, 60 );
+	    g.setColor(Color.red);
+	    g.drawString("van Daal", 50, 75);
+	  }
+	}
